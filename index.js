79c2614aa21f1413d0f16ed828721c563f379ca5
Zleub <debray.arnaud@gmail.com>
2016-12-16 23:11:16 +0100

    mainThread

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ let win
 
 function createWindow () {
 	win = new BrowserWindow({
-		width: 800,
-		height: 600,
+		width: 800 * 2,
+		height: 600 * 2,
 		useContentSize: true,
 		resizable: true
 	})
 
 	win.loadURL(url.format({
-		pathname: path.join(__dirname, 'index.html'),
+		pathname: path.join(__dirname, 'render/index.html'),
 		protocol: 'file:',
 		slashes: true
 	}))
 
-	// win.webContents.openDevTools()
+	win.webContents.openDevTools()
 
 	win.on('app-command', (e, cmd) => {
 		console.log(e, cmd)
