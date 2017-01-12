67495aa1775e9c79e8672d9d71e9a2e13d4956de
Zleub <debray.arnaud@gmail.com>
2017-01-12 22:33:00 +0100

    Job work

--- render/scripts/config.js
+++ render/scripts/config.js
@@ -1,23 +1,50 @@
+let JP_table = {
+    "Freelancer":  20,
+    "Warrior":     14,
+    "Monk":        14,
+    "White Mage":  10,
+    "Black Mage":  10,
+    "Red Mage":    12,
+    "Thief":       18,
+    "Ranger":      14,
+    "Knight":      12,
+    "Scholar":     24,
+    "Geomancer":   14,
+    "Viking":      14,
+    "Dragoon":     16,
+    "Dark Knight": 14,
+    "Evoker":      10,
+    "Bard":        18,
+    "Black Belt":  14,
+    "Magus":       10,
+    "Devout":      10,
+    "Sage":        10,
+    "Summoner":    12,
+    "Ninja":       12,
+    "Onion Knight": 8
+}
+
+let Level_table = [
+	16, 31, 58, 99, 159, 239, 343, 473, 631, 821,
+    1044, 1304, 1601, 1940, 2321, 2748, 3222, 3745, 4321, 4950,
+    5636, 6380, 7185, 8051, 8982, 9980, 11046, 12183, 13393, 14676,
+    16037, 17475, 18995, 20596, 22282, 24053, 25913, 27863, 29904, 32039,
+    34269, 36597, 39024, 41552, 44183, 46918, 49760, 52710, 55769, 58941,
+    62226, 65627, 69144, 72781, 76538, 80417, 84420, 88549, 92805, 97191,
+    101707, 106356, 111139, 116058, 121115, 126311, 131648, 137128, 142752, 148522,
+    154439, 160506, 166724, 173095, 179619, 186299, 193137, 200134, 207292, 214611,
+    222095, 229744, 237561, 245546, 253701, 262028, 270529, 279205, 288057, 297087,
+    306298, 315689, 325264, 335023, 344968, 355100, 365422, 375934
+]
+
 let config = {
-	jobs: {
-		'Freelancer': (c) => {
-			if (Freelancer.stats[c])
-				return Freelancer.stats[c]
-			else {
-				let i = Object.keys(Freelancer.stats).reduce( (p, k) => {
-					if (Number(k) < p)
-						return k
-					return p
-				}, c)
-				if (!Freelancer.stats[i])
-					return Freelancer.stats[1]
-				else
-					return Freelancer.stats[i]
-			}
-		}
-
-
+	JP_Gain: (character) => {
+		if (character.joblvl() > 14)
+			return JP_table[character.attrs.job.name]
+		else
+			return 20
 	},
+
 	Number_of_Hits: (character) => {
 		return {
 			left: parseInt( 1
