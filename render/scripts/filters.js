6ce2e1ede9ab9f235853536c48c8c10d6741cd3e
Zleub <debray.arnaud@gmail.com>
2017-03-11 20:22:48 +0100

    refactoring IV

--- render/scripts/filters.js
+++ render/scripts/filters.js
@@ -1,35 +1,101 @@
+let harmonizer = new Harmonizer()
+let divizer = []
+for (var i = 0; i < 360; i += 10) {
+	divizer.push(i)
+}
+let colors = harmonizer.harmonize('#801515', divizer)
+
 let test_data = [
 	(x, y) => Math.cos(x * x + y * y),
-	// (x, y) => Math.sin(x * x + y * y),
-	// (x, y) => Math.tan(x * x + y * y),
-	// (x, y) => Math.sin(x ^ 2 + y),
+	(x, y) => Math.sin(x * x + y * y),
+	(x, y) => Math.tan(x * x + y * y),
+	(x, y) => Math.cos(x * x - y * y),
+	(x, y) => Math.sin(x * x - y * y),
+	(x, y) => Math.tan(x * x - y * y),
+	(x, y) => Math.cos(x * x * y * y),
+	(x, y) => Math.sin(x * x * y * y),
+	(x, y) => Math.tan(x * x * y * y),
+	// (x, y) => Math.cos((x * x) / (y * y)),
+	// (x, y) => Math.sin((x * x) / (y * y)),
+	// (x, y) => Math.tan((x * x) / (y * y)),
+	(x, y) => Math.cos(x * y + x * y),
+	(x, y) => Math.sin(x * y + y * x),
+	(x, y) => Math.tan(x * y + y * x),
+	// (x, y) => Math.cos(x * y - y * x),
+	// (x, y) => Math.sin(x * y - y * x),
+	// (x, y) => Math.tan(x * y - y * x),
+	(x, y) => Math.cos(x * y * y * x),
+	(x, y) => Math.sin(x * y * y * x),
+	(x, y) => Math.tan(x * y * y * x),
+	// (x, y) => Math.cos((x * y) / (y * x)),
+	// (x, y) => Math.sin((x * y) / (y * x)),
+	// (x, y) => Math.tan((x * y) / (y * x)),
 ]
 
-let test = function (imageData) {
-	let res = 10
-	let {width, height, data} = imageData
-	let f = test_data[Math.floor(Math.random() * test_data.length)]
+let get_color = (old) => {
+	var color = colors[ Math.floor(Math.random() * colors.length) ]
+	if (color == old)
+		return get_color(old)
+	else
+		return color
+}
 
-	let w = width * 2
-	let h = height * 2
+let test = function (imageData) {
+	var res = 20
+	var {width, height, data} = imageData
+	var f = test_data[Math.floor(Math.random() * test_data.length)]
+	var color = get_color()
+	var r = one.color(color).red() * 255
+	var g = one.color(color).green() * 255
+	var b = one.color(color).blue() * 255
+
+	console.log(width, height)
+
+	var w = width * 2
+	var h = height * 2
 	for (var x = -w; x < w; x += 4 * res) {
 		for (var y = -h; y < h; y += 4 * res) {
-			let c = Math.round( f(x, y) )
-			if (c > 0) {
+			let c = Math.round( f(x + res * 2, y + res * 2) )
 				for (var i = 0; i < res * 4; i += 4) {
 					for (var j = 0; j < res * 4; j += 4) {
 						let _x = x + i + w
 						let _y = (y + j + h) * width
 
-						console.log(Math.atan( y / x ))
-						console.log(one.color(`hsla(${Math.atan( y / x )}, 75%, 75%, .1)`).red())
-						data[_x + _y] = 0
-						data[_x + _y + 1] = 0
-						data[_x + _y + 2] = 0
-						data[_x + _y + 3] = 255
+						data[_x + _y + 0] = r
+						data[_x + _y + 1] = g
+						data[_x + _y + 2] = b
+						data[_x + _y + 3] = Math.abs(c * 255)
 					}
 				}
-			}
+		}
+	}
+
+	var res = res / 4
+	var {width, height, data} = imageData
+	var f = test_data[Math.floor(Math.random() * test_data.length)]
+	var color = get_color(color)
+	var r = one.color(color).red() * 255
+	var g = one.color(color).green() * 255
+	var b = one.color(color).blue() * 255
+
+	var w = width * 2
+	var h = height * 2
+	for (var x = -w; x < w; x += 4 * res) {
+		for (var y = -h; y < h; y += 4 * res) {
+			let c = Math.round( f(x + res * 2, y + res * 2) )
+				for (var i = 0; i < res * 4; i += 4) {
+					for (var j = 0; j < res * 4; j += 4) {
+						let _x = x + i + w
+						let _y = (y + j + h) * width
+
+						if (Math.abs(c * 255) > 127) {
+							data[_x + _y + 0] = r
+							data[_x + _y + 1] = g
+							data[_x + _y + 2] = b
+							data[_x + _y + 3] = Math.abs(c * 255)
+						}
+					}
+				}
 		}
 	}
 }
